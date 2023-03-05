2c919afb6e443bd461f9c6949ea60695a6d94b25
booiris <954520770@qq.com>
2023-03-05 20:31:05 +0800

    feat: fix type error

--- src-tauri/src/model/msg_info.rs
+++ src-tauri/src/model/msg_info.rs
@@ -3,10 +3,16 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MsgInfo {
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub _id: Option<String>,
+    pub _id: Option<MongoDBDataID>,
     pub sender_id: i64,
     pub receiver_id: i64,
     pub chat_role: openai_api::api::ChatRole,
     pub content: String,
     pub time: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MongoDBDataID {
+    #[serde(rename="$oid")]
+    pub oid: String,
 }
